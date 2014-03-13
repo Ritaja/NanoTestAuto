a40903bca2ed51a18bb7bb6890287f9c6f50359e
ritaja sengupta <ritaja.sengupta@gmail.com>
2014-03-13 17:21:05 +0100

    Architecture revamp:: generic class added

--- src/testControl/ControlImpl.java
+++ src/testControl/ControlImpl.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.HashMap;
 
 import excelReader.ReaderImpl;
+import testGeneric.GenericTest;
 import testImplementation.TestCases;
 import testNavigation.Navigation;
 
@@ -18,36 +19,59 @@ public class ControlImpl{
 	 */
 	private ReaderImpl reader;
 	private TestCases testCases;
+	private GenericTest genericTest;
 	private HashMap parameters;
 	private HashMap navigationComponents;
 	public ControlImpl() throws IOException
 	{
 		this.reader = new ReaderImpl();
 		this.testCases = new TestCases();
+		this.genericTest = new GenericTest();
 		this.parameters = null;
 		this.navigationComponents = null;
 	}
     
     public void getParameters()
     {
-		this.parameters = testCases.getParameters();
+		this.parameters = this.testCases.getParameters();
     	
     }
     
     public void getNavigationComponents()
     {
-		this.navigationComponents = testCases.getNavigationComponents();
+		this.navigationComponents = this.testCases.getNavigationComponents();
     	
     }
-	
     
+    public void setParameters()
+    {
+    	this.genericTest.setParameters(this.parameters);
+    }
+    
+    public void setNavigationComponents()
+    {
+    	this.genericTest.setNavigationComponents(this.navigationComponents);
+    }
+    
+    public void navigateTest()
+    {
+    	this.genericTest.Navigate();
+    }
+	
+    public void  assertTest()
+    {
+    	this.genericTest.assertTest();
+    }
     
 	public static void main(String[] args) throws IOException {
 		
 		ControlImpl control = new ControlImpl();
 		control.getNavigationComponents();
 		control.getParameters();
-		
+		control.setParameters();
+		control.setNavigationComponents();
+		control.navigateTest();
+        control.assertTest();
 		
 		
 
--- src/testGeneric/GenericTest.java
+++ src/testGeneric/GenericTest.java
@@ -1,5 +1,37 @@
 package testGeneric;
 
+import java.util.HashMap;
+
 public class GenericTest {
+	
+	private HashMap parameters;
+	private HashMap navigationComponents;
+	
+	public GenericTest()
+	{
+		this.parameters = null;
+		this.navigationComponents = null;
+	}
+
+	public void Navigate() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void assertTest() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setParameters(HashMap parameters) {
+		// TODO Auto-generated method stub
+		this.parameters = parameters;
+	}
+
+	public void setNavigationComponents(HashMap navigationComponents) {
+		// TODO Auto-generated method stub
+		
+		this.navigationComponents = navigationComponents;
+	}
 
 }
